38012cdc53ce85738dc8ea3e3283a5905edd0b14
Ravisher Singh <60622194+ravishersingh2@users.noreply.github.com>
2021-02-27 20:58:57 -0700

    Test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var http = require('http');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
-db = require('./db')(); //global hack
-var jwt = require('jsonwebtoken');
+var express = require("express");
+var http = require("http");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var authController = require("./auth");
+var authJwtController = require("./auth_jwt");
+db = require("./db")(); //global hack
+var jwt = require("jsonwebtoken");
 
 var app = express();
 app.use(bodyParser.json());
@@ -17,9 +17,9 @@ var router = express.Router();
 
 function getJSONObject(req) {
     var json = {
-        headers : "No Headers",
+        headers: "No Headers",
         key: process.env.UNIQUE_KEY,
-        body : "No Body"
+        body: "No Body"
     };
 
     if (req.body != null) {
@@ -32,100 +32,151 @@ function getJSONObject(req) {
     return json;
 }
 
-router.route('/post')
-    .post(authController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
-            var o = getJSONObject(req);
-            res.json(o);
-        }
-    );
-
-router.route('/postjwt')
-    .post(authController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
-            res.send(req.body);
-        }
-    );
-
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: '🚨ERROR. No user name or password entered 🕵.'});
-    } else {
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password
-        };
-        // save the user
-        db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successful created new user.'});
+router.route("/post").post(authController.isAuthenticated, function(req, res) {
+    console.log(req.body);
+    res = res.status(200);
+    if (req.get("Content-Type")) {
+        console.log("Content-Type: " + req.get("Content-Type"));
+        res = res.type(req.get("Content-Type"));
     }
+    var o = getJSONObject(req);
+    res.json(o);
 });
 
-router.post('/signin', function(req, res) {
-
-    var user = db.findOne(req.body.username);
-
-    if (!user) {
-        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-    }
-    else {
-        // check if password matches
-        if (req.body.password == user.password)  {
-            var userToken = { id : user.id, username: user.username };
-            var token = jwt.sign(userToken, process.env.UNIQUE_KEY);
-            res.json({success: true, token: 'JWT ' + token});
+router
+    .route("/movies")
+    .post(function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get("Content-Type")) {
+            console.log("Content-Type: " + req.get("Content-Type"));
+            res = res.type(req.get("Content-Type"));
         }
-        else {
-            res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+        res.send({
+            status: 200,
+            message: "movie saved",
+            headers: req.headers,
+            query: req.query,
+            env: process.env.UNIQUE_KEY
+        });
+    })
+    .get(function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get("Content-Type")) {
+            console.log("Content-Type: " + req.get("Content-Type"));
+            res = res.type(req.get("Content-Type"));
         }
-    }
-});
-
-router.route('/movies')
-    .get(function (req, res) {
-        res.json({status: 200, msg: "Get movies", headers: req.headers, query: req.query, env: process.env.UNIQUE_KEY});
+        res.send({
+            status: 200,
+            message: "GET movies",
+            headers: req.headers,
+            query: req.query,
+            env: process.env.UNIQUE_KEY
+        });
     })
-
-    .post(function (req, res) {
-        res.json({status: 200, msg: "movie saved", headers: req.headers, query: req.query, env: process.env.UNIQUE_KEY});
-    })
-
     .put(authJwtController.isAuthenticated, function(req, res) {
-        res.json({status: 200, msg: "movie updated", headers: req.headers, query: req.query, env: process.env.UNIQUE_KEY});
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get("Content-Type")) {
+            console.log("Content-Type: " + req.get("Content-Type"));
+            res = res.type(req.get("Content-Type"));
+        }
+        res.send({
+            status: 200,
+            message: "movie updated",
+            headers: req.headers,
+            query: req.query,
+            env: process.env.UNIQUE_KEY
+        });
     })
+    .delete(authController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get("Content-Type")) {
+            console.log("Content-Type: " + req.get("Content-Type"));
+            res = res.type(req.get("Content-Type"));
+        }
+        res.send({
+            status: 200,
+            message: "movie deleted",
+            headers: req.headers,
+            query: req.query,
+            env: process.env.UNIQUE_KEY
+        });
+    })
+    .all(function(req, res) {
+        console.log(req.body);
+        res = res.status(403);
+        res.send(
+            "HTTP method not supported: only GET, POST, PUT, and DELETE requests are supported"
+        );
+    });
 
-    .delete(function (req, res) {
-        var user = db.findOne(req.body.username);
-        if(!user) {
-            res.status(401).send({success: false, msg: "Authentication failed. User not found"});
+router
+    .route("/signup")
+    .post(function(req, res) {
+        if (!req.body.username || !req.body.password) {
+            res.json({ success: false, msg: "Please pass username and password." });
         } else {
-            if(req.body.password === user.password){
-                res.json({status: 200, message: "Movie Deleted", headers: req.headers, query: req.query,env: process.env.UNIQUE_KEY});
-            }
-            else{
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+            var newUser = {
+                username: req.body.username,
+                password: req.body.password
+            };
+            // save the user
+            db.save(newUser); //no duplicate checking
+            res.json({ success: true, msg: "Successful created new user." });
+        }
+    })
+    .all(function(req, res) {
+        console.log(req.body);
+        res = res.status(403);
+        res.send("HTTP method not supported: only POST request is supported");
+    });
+
+router
+    .route("/signin")
+    .post(function(req, res) {
+        var user = db.findOne(req.body.username);
+
+        if (!user) {
+            res
+                .status(401)
+                .send({
+                    success: false,
+                    msg: "Authentication failed. User not found."
+                });
+        } else {
+            // check if password matches
+            if (req.body.password === user.password) {
+                var userToken = { id: user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                res.json({ success: true, token: "JWT " + token });
+            } else {
+                res
+                    .status(401)
+                    .send({
+                        success: false,
+                        msg: "Authentication failed. Wrong password."
+                    });
             }
         }
+    })
+    .all(function(req, res) {
+        console.log(req.body);
+        res = res.status(403);
+        res.send("HTTP method not supported: only POST request is supported");
     });
 
-//All other routs and methods
-router.all('*', function(req, res) {
-    res.json({
-        error: 'Your HTTP method is not supported. Fix it please.👮‍'
-    });
+router.route("/").all(function(req, res) {
+    console.log(req.body);
+    res = res.status(403);
+    res.send("ROOT FAIL! Invalid path/directory!");
 });
 
-app.use('/', router);
-app.listen(process.env.PORT || 80);
+app.use("/", router);
+app.listen(process.env.PORT || 8080);
 
-module.exports = app;
+module.exports = app; // for testing
+
+//test
